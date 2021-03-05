5d029a0cd389bd646339f2456c97d540483a902c
wuzhiwei-545 <wuzhiwei545@outlook.com>
2021-03-05 16:31:14 +0800

    node 收邮件

--- server.js
+++ server.js
@@ -92,3 +92,79 @@ function onError (err) {
       throw err
   }
 }
+
+const Imap = require('imap')
+const MailParser = require('mailparser').MailParser
+const fs = require('fs')
+
+const imap = new Imap({
+  user: '389057465@qq.com',
+  password: 'mdsblpxpjtkvbjde',
+  host: 'imap.qq.com',
+  port: 993,
+  tls: true,
+  tlsOptions: { rejectUnauthorized: false } // 禁用对证书有效性的检查
+})
+
+function openInbox(cb) {
+  imap.openBox('INBOX', true, cb)
+}
+
+imap.once('ready', () => {
+  openInbox((err, box) => {
+    console.log('打开邮箱')
+    if (err) throw err
+    imap.search(['UNSEEN', ['SINCE', 'May 5, 2020']], (error, res) => {
+      if (error) throw error
+      const f = imap.fetch(res, { bodies: '' }) // 抓取邮件
+      f.on('message', (msg, seqno) => {
+        const mailparser = new MailParser()
+        msg.on('body', (stream, info) => {
+          stream.pipe(mailparser) // 将为解析的数据流pipe到mailparser
+
+          // 邮件头内容
+          mailparser.on('headers', headers => {
+            console.log('邮件头信息>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+            console.log(`邮件主题 ${headers.get('subject')}`)
+            console.log(`发件人 ${headers.get('from').text}`)
+            console.log(`收件人 ${headers.get('to').text}`)
+          })
+
+          // 邮件内容
+          mailparser.on('data', data => {
+            if (data.type === 'text') { // 邮件正文
+              console.log('邮件内容信息>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+              console.log(`邮件内容 ${data.html}`)
+            }
+            if (data.type === 'attachment') { // 附件
+              console.log('邮件附件信息>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+              console.log(`附件名称：${data.filename}`)
+              // data.content.pipe(fs.createWriteStream(data.filename)) // 保存附件到当前目录下
+              // data.release()
+            }
+          })
+        })
+        msg.once('end', () => {
+          console.log(`${seqno}完成`)
+        })
+      })
+      f.once('error', err => {
+        console.log(`抓取出现错误：${err}`)
+      })
+      f.once('end', () => {
+        console.log('所有邮件抓取完成')
+        imap.end()
+      })
+    })
+  })
+})
+
+imap.once('error', err => {
+  console.log(`imap err ${err}`)
+})
+
+imap.once('end', () => {
+  console.log('关闭邮箱')
+})
+
+imap.connect()
