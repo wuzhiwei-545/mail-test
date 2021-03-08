5ec9b13965d880bc3814e41d10171942be25573c
wuzhiwei-545 <wuzhiwei545@outlook.com>
2021-03-08 17:58:05 +0800

    vw配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 module.exports = {
+  chainWebpack: (config) => {
+    config.module
+    .rule('vue')
+    .test(/\.vue$/)
+    .use('style-vw-loader')
+    .loader('style-vw-loader')
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-px-to-viewport')({
+            unitToConvert: 'px',
+            viewportWidth: 375,
+            unitPrecision: 3,
+            porpList: ['*'],
+            viewportUnit: 'vw',
+            fontViewportUnit: 'vw',
+            selectorBlackList: [],
+            minPixelValue: 1,
+            mediaQuery: false,
+            replace: true,
+            exclude: /(\/|\\)(node_modules)(\/|\\)/
+          })
+        ]
+      }
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
